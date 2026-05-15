7efb6872617379b59934ea524b00a6a3a9f5f134
Sherwin Burgess <sherwinburgess@gmail.com>
2026-05-15 21:51:36 +0100

    add backend project

--- frontend/src/Pages/Home.tsx
+++ frontend/src/Pages/Home.tsx
@@ -1,8 +1,25 @@
-export default function Home() {
-    return (
-        <div>
-            <h1 className="text-5xl text-center">Welcome Home !!</h1>
+import { useState } from "react";
 
+export default function Home() {
+    const labels = new Array(11).fill(0).map((_, i) => i);
+    const [rangeValue, setRangeValue] = useState(0);
+    return (
+        <div className="space-y-8">
+            <h1 className="text-5xl text-center">Welcome Home !!</h1>
+            <div className="mx-auto w-[400px]">
+                <p className="text-xl text-center">
+                    {rangeValue}
+                </p>
+                <input className="w-full"
+                    value={rangeValue}
+                    onChange={(e) => setRangeValue(parseFloat(e.target.value))}
+                    type="range" min={0} max={10} step={0.1} list="markers" />
+                <datalist id="markers" className="flex justify-between">
+                    {labels.map(label =>
+                        <option key={label} value={label} label={label.toString()}></option>
+                    )}
+                </datalist>
+            </div>
         </div>
     )
 }
